b20a382feed005af65d45fef7699f10f6cac9b19
ksilvaf <ferreirakevin325@gmail.com>
2024-10-01 02:37:20 -0300

    test: vercel

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -1,5 +1,7 @@
 
+import { Button } from "@/components/ui/button";
 import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
@@ -7,6 +9,8 @@ export default function Home() {
       <CardHeader>
         <CardTitle className="text-2xl font-bold">Bem vindo a Pethost</CardTitle>
         <CardDescription className="text-sm mt-2">Faça parte da nossa comunidade de anfitriões</CardDescription>
+        <Input className="mt-4" placeholder="Digite seu e-mail" />
+        <Button className="mt-4">Cadastrar</Button>
       </CardHeader>
     </Card>
   );
