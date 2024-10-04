289b94b9589f628be6114fc2bc373ee26436a662
Allan kelven <166170445+allankdev@users.noreply.github.com>
2024-10-04 00:21:16 -0300

    Criação do  Componente como funciona

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -1,4 +1,5 @@
 import { CarouselDemo } from "./components/Carrousel";
+import ComoFunciona from "./components/Comofunciona";
 import { Navbar } from "./components/Navbar";
 
 
@@ -7,6 +8,7 @@ export default function Home() {
     <>
     <Navbar />
     <CarouselDemo />
+    <ComoFunciona />
     </>
   );
 }
