ce82258e9740b67790c88d6ce6135e30fb6bc9d3
Nailton S. <44982114+nailtonsimoes@users.noreply.github.com>
2024-10-04 00:32:04 -0300

    feat_faq: implementação de footer

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -1,5 +1,6 @@
 import { CarouselDemo } from "./components/Carrousel";
 import Faq from "./components/Faq";
+import Footer from "./components/Footer";
 import { Navbar } from "./components/Navbar";
 
 
@@ -9,6 +10,7 @@ export default function Home() {
     <Navbar />
     <CarouselDemo />
     <Faq />
+    <Footer />
     </>
   );
 }
